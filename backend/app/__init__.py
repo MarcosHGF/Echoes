611d7f82b2db8476cf2cb4acd048bec2ea5bda6d
CesarHPMP <cesar.melo@sga.pucminas.br>
2025-02-28 18:58:42 -0300

    postgres integrations

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_migrate import Migrate
 from app.extensions import db
 
 def create_app():
@@ -8,7 +9,14 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Evita warning desnecessário
 
+    # Configuração para PostgresSQL
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://usuario:senha@localhost:5433/nome_do_banco'
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+
     db.init_app(app)
+    migrate = Migrate(app, db)  # Initialize Flask-Migrate
     
     # Import and register blueprints
     from app.routes import likes_bp, users_bp, profile_bp, login_bp, posts_bp, spotify_login, tracks_bp, userposts_bp, getPostsUser_bp, add_follower_bp
